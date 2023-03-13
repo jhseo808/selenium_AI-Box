0d0112f46d21e0cb0497d809a67c754bacd04d78
jhseo808 <121909004+jhseo808@users.noreply.github.com>
2023-03-13 20:41:40 +0900

    Update 03.update_auto.py

--- 03.update_auto.py
+++ 03.update_auto.py
@@ -17,41 +17,26 @@ driver = webdriver.Chrome(service=service, options=options)
 driver.implicitly_wait(3)
 driver.maximize_window()
 
-url = 'http://172.16.4.114/web/#/web/auth'
+url = 'http://172.16.6.230/web/#/web/auth'
 driver.get(url)
 action = ActionChains(driver)
 
-#로그인
 driver.find_element(By.CLASS_NAME, 'mat-form-field-infix')
-(
-action.send_keys('admin').key_down(Keys.TAB)
-.send_keys('Pass0001!').pause(1).key_down(Keys.ENTER).perform()
-)
+(action.send_keys('admin').key_down(Keys.TAB).send_keys('Pass0001!').pause(1).key_down(Keys.ENTER).perform())
 action.reset_actions
 time.sleep(1)
+print('Login OK')
 
-#업그레이드 페이지 이동
-url = "http://172.16.4.114/web/#/web/upgrade"
-driver.get(url)
-
-#파일선택
-file_btn = driver.find_element(By.CSS_SELECTOR,"body > app-root > app-sidenav-responsive > div > mat-sidenav-container > mat-sidenav-content > app-upgrade > div > mat-card > mat-list:nth-child(2) > mat-card > div:nth-child(4) > div:nth-child(2) > button > span")
-file_btn.click()
-
-#pyautogui.moveTo(554, 946, 1)
-#pyautogui.doubleClick()
-#pyautogui.click()
-
-time.sleep(1)
-pyautogui.typewrite('D:\VIXAI-120G-0200-1.5.14.img')
-time.sleep(2)
+driver.get("http://172.16.6.230/web/#/web/upgrade")
+btn_file = driver.find_element(By.CSS_SELECTOR,"body > app-root > app-sidenav-responsive > div > mat-sidenav-container > mat-sidenav-content > app-upgrade > div > mat-card > mat-list:nth-child(2) > mat-card > div:nth-child(4) > div:nth-child(2) > button > span")
+btn_file.click()
+time.sleep(1) # 파일 선택 폴더 생성 대기 시간 필요!
+pyautogui.typewrite('D:\VIXAI-120G-0200-1.5.14.img') # 업그레이드 파일명
+time.sleep(2) # 파일명 작성 시간 대기 필요!
 pyautogui.press('enter')
 time.sleep(2)
-#pyautogui.moveTo(1222, 879, 1)
-#pyautogui.click()
-
-upgrade_btn = driver.find_element(By.CSS_SELECTOR, "body > app-root > app-sidenav-responsive > div > mat-sidenav-container > mat-sidenav-content > app-upgrade > div > mat-card > mat-list:nth-child(2) > mat-card > div:nth-child(5) > app-btn-upgrade > button > span")
-upgrade_btn.click()
+btn_upgrade = driver.find_element(By.CSS_SELECTOR, "body > app-root > app-sidenav-responsive > div > mat-sidenav-container > mat-sidenav-content > app-upgrade > div > mat-card > mat-list:nth-child(2) > mat-card > div:nth-child(5) > app-btn-upgrade > button > span")
+btn_upgrade.click()
 time.sleep(2)
 #upgrade_popup_btn = driver.find_element(By.CSS_SELECTOR, '#mat-dialog-0 > app-confirm-dialog > div.mat-dialog-actions > button.mat-raised-button.mat-primary > span')
 #upgrade_popup_btn.click()
