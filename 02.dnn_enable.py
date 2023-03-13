6407ce79a2130fae061ec4e9e0848c280d073007
jhseo808 <121909004+jhseo808@users.noreply.github.com>
2023-03-13 20:35:15 +0900

    Update 02.dnn_enable.py

--- 02.dnn_enable.py
+++ 02.dnn_enable.py
@@ -16,30 +16,25 @@ driver = webdriver.Chrome(service=service, options=options)
 driver.implicitly_wait(3)
 driver.maximize_window()
 
-url = 'http://172.16.4.114/web/#/web/auth'
-driver.get(url)
+driver.get('http://172.16.6.230/web/#/web/auth')
 action = ActionChains(driver)
 
 driver.find_element(By.CLASS_NAME, 'mat-form-field-infix')
-(
-action.send_keys('admin').key_down(Keys.TAB)
-.send_keys('Pass0001!').pause(1).key_down(Keys.ENTER).perform()
-)
+(action.send_keys('admin').key_down(Keys.TAB).send_keys('Pass0001!').pause(1).key_down(Keys.ENTER).perform())
 action.reset_actions
 time.sleep(1)
+print('Login OK\n----------------------------------------------------------------')
 
-#dnn 페이지 이동
-url = 'http://172.16.4.114/web/#/web/ai-dnn-resource'
-driver.get(url)
-
-#검출기 활성화
+driver.get('http://172.16.6.230/web/#/web/ai-dnn-resource') # DNN 자원 페이지
+# 검출기 활성화
 driver.find_element(By.CSS_SELECTOR, '#mat-checkbox-3 > label > span').click()
 driver.find_element(By.CSS_SELECTOR, '#mat-checkbox-4 > label > span').click()
-#driver.find_element(By.CSS_SELECTOR, '#mat-checkbox-5 > label > span').click()
+# driver.find_element(By.CSS_SELECTOR, '#mat-checkbox-5 > label > span').click() # default로 활성화
 driver.find_element(By.CSS_SELECTOR, '#mat-checkbox-6 > label > span').click()
 driver.find_element(By.CSS_SELECTOR, '#mat-checkbox-7 > label > span').click()
+print('DNN Detector All complete!\n----------------------------------------------------------------')
 
-#인식모듈 활성화
+# 인식모듈 활성화
 driver.find_element(By.CSS_SELECTOR, '#mat-checkbox-8 > label > span').click()
 driver.find_element(By.CSS_SELECTOR, '#mat-checkbox-9 > label > span').click()
 driver.find_element(By.CSS_SELECTOR, '#mat-checkbox-10 > label > span').click()
@@ -47,6 +42,9 @@ driver.find_element(By.CSS_SELECTOR, '#mat-checkbox-11 > label > span').click()
 driver.find_element(By.CSS_SELECTOR, '#mat-checkbox-12 > label > span').click()
 driver.find_element(By.CSS_SELECTOR, '#mat-checkbox-13 > label > span').click()
 driver.find_element(By.CSS_SELECTOR, '#mat-checkbox-14 > label > span').click()
+print('DNN Recognition-Module All complete!\n----------------------------------------------------------------')
 
-driver.find_element(By.CSS_SELECTOR, 'body > app-root > app-sidenav-responsive > div > mat-sidenav-container > mat-sidenav-content > app-ai-dnn-resource > div > mat-card > div:nth-child(4) > app-btn-apply > button > span').click()
+driver.find_element(
+    By.CSS_SELECTOR, 'body > app-root > app-sidenav-responsive > div > mat-sidenav-container > mat-sidenav-content > app-ai-dnn-resource > div > mat-card > div:nth-child(4) > app-btn-apply > button > span'
+    ).click() # DNN 자원 적용
 
