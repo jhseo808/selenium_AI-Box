ba42733ab979631cfe3a3ff342e603423e13d776
jhseo808 <121909004+jhseo808@users.noreply.github.com>
2023-03-13 20:37:02 +0900

    Update 05.network.py

--- 05.network.py
+++ 05.network.py
@@ -17,40 +17,32 @@ driver = webdriver.Chrome(service=service, options=options)
 driver.implicitly_wait(3)
 driver.maximize_window()
 
-url = 'http://172.16.4.114/web/#/web/auth'
-driver.get(url)
+driver.get('http://172.16.6.230/web/#/web/auth')
 action = ActionChains(driver)
 
-#로그인
 driver.find_element(By.CLASS_NAME, 'mat-form-field-infix')
-(
-action.send_keys('admin').key_down(Keys.TAB)
-.send_keys('Pass0001!').pause(1).key_down(Keys.ENTER).perform()
-)
+(action.send_keys('admin').key_down(Keys.TAB).send_keys('Pass0001!').pause(1).key_down(Keys.ENTER).perform())
 action.reset_actions
 time.sleep(1)
+print('Login OK\n----------------------------------------------------------------')
 
-#네트워크 페이지
-url = 'http://172.16.4.114/web/#/web/network-setting'
-driver.get(url)
+driver.get('http://172.16.6.230/web/#/web/network-setting') # 네트워크 페이지
 time.sleep(1)
-
-network_DHCP = driver.find_element(By.CSS_SELECTOR, '#mat-button-toggle-26-button')
-network_DHCP.click()
+driver.find_element(By.CSS_SELECTOR, '#mat-button-toggle-26-button').click() # 네트워크 DHCP
+print('Network DHCP complete!\n----------------------------------------------------------------')
 time.sleep(0.5)
-network_manual = driver.find_element(By.ID, 'mat-button-toggle-25')
-network_manual.click()
+driver.find_element(By.ID, 'mat-button-toggle-25').click() # 네트워크 수동
 pyautogui.scroll(-300)
 
-#네트워크 수동 입력
 network_set = driver.find_element(By.CSS_SELECTOR, '#mat-input-12')
 network_set.clear()
 network_set.click()
-(
-    action.send_keys('172.16.4.114').key_down(Keys.TAB).send_keys('255.255.255.0').key_down(Keys.TAB).send_keys('172.16.4.1').key_down(Keys.TAB).send_keys('1.1.1.1').key_down(Keys.TAB)
+( #네트워크 수동 입력
+    action.send_keys('172.16.6.230').key_down(Keys.TAB).send_keys('255.255.255.0').key_down(Keys.TAB).send_keys('172.16.6.1').key_down(Keys.TAB).send_keys('1.1.1.1').key_down(Keys.TAB)
     .send_keys('80').key_down(Keys.TAB).send_keys('554').perform()
 )
 action.reset_actions
 time.sleep(1)
 network_apply = driver.find_element(By.CSS_SELECTOR, 'body > app-root > app-sidenav-responsive > div > mat-sidenav-container > mat-sidenav-content > app-network-setting > div > mat-card > div:nth-child(6) > app-btn-apply > button > span')
 #network_apply.click()
+print('Network Static complete!\n----------------------------------------------------------------')
