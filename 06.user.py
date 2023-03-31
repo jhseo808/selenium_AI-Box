1d90c48ba31ff0805f1bc90edf4840cc61ee7c25
JH$EO <121909004+jhseo808@users.noreply.github.com>
2023-03-31 19:44:41 +0900

    Update 06.user.py

--- 06.user.py
+++ 06.user.py
@@ -37,8 +37,8 @@ action.send_keys('Pass0001!!').perform()
 driver.find_element(By.ID, 'mat-input-15').click() #  새 비밀번호 확인
 action.send_keys('Pass0001!!').perform()
 
-btn_apply = driver.find_element(By.CSS_SELECTOR, 'body > app-root > app-sidenav-responsive > div > mat-sidenav-container > mat-sidenav-content > app-user-setting > div > mat-card > div:nth-child(4) > app-btn-apply > button')
-btn_apply.click()
+btn_user_apply = driver.find_element(By.CSS_SELECTOR, 'body > app-root > app-sidenav-responsive > div > mat-sidenav-container > mat-sidenav-content > app-user-setting > div > mat-card > div:nth-child(4) > app-btn-apply > button')
+btn_user_apply.click()
 print('Password change complte!\n----------------------------------------------------------------')
 btn_logout = driver.find_element(By.CSS_SELECTOR, 'body > app-root > app-sidenav-responsive > div > mat-toolbar > button:nth-child(4)')
 btn_logout.click()
@@ -49,39 +49,42 @@ action.reset_actions
 time.sleep(1)
 '''
 
-pyautogui.scroll(-300)
-btn_add = driver.find_element(By.CSS_SELECTOR, 'body > app-root > app-sidenav-responsive > div > mat-sidenav-container > mat-sidenav-content > app-user-setting > div > mat-card > mat-list:nth-child(3) > mat-card > div.flex-row > div.ng-star-inserted > app-btn-add')
-btn_add.click() # 사용자 11개 추가
-btn_add.click()
-btn_add.click()
-btn_add.click()
-btn_add.click()
-btn_add.click()
-btn_add.click()
-btn_add.click()
-btn_add.click()
-btn_add.click()
-add_user = driver.find_element(By.ID, 'mat-input-16')
-add_user.clear()
-add_user.click()
+# 사용자 계정 10개 추가
+for i in range(10):
+    btn_user_add = driver.find_element(By.CSS_SELECTOR, 'body > app-root > app-sidenav-responsive > div > mat-sidenav-container > mat-sidenav-content > app-user-setting > div > mat-card > mat-list:nth-child(3) > mat-card > div.flex-row > div.ng-star-inserted > app-btn-add')
+    btn_user_add.click()
+
+user_input_text = driver.find_element(By.ID, 'mat-input-16')
+user_input_text.clear()
+user_input_text.click()
 time.sleep(1)
-(
-    action.send_keys('qatest1').key_down(Keys.TAB).send_keys('Pass0001!').key_down(Keys.TAB).key_down(Keys.TAB).key_down(Keys.TAB).key_down(Keys.TAB)
-    .send_keys('qatest2').key_down(Keys.TAB).send_keys('Pass0001!').key_down(Keys.TAB).key_down(Keys.TAB).key_down(Keys.TAB).key_down(Keys.TAB)
-    .send_keys('qatest3').key_down(Keys.TAB).send_keys('Pass0001!').key_down(Keys.TAB).key_down(Keys.TAB).key_down(Keys.TAB).key_down(Keys.TAB)
-    .send_keys('qatest4').key_down(Keys.TAB).send_keys('Pass0001!').key_down(Keys.TAB).key_down(Keys.TAB).key_down(Keys.TAB).key_down(Keys.TAB)
-    .send_keys('qatest5').key_down(Keys.TAB).send_keys('Pass0001!').key_down(Keys.TAB).key_down(Keys.TAB).key_down(Keys.TAB).key_down(Keys.TAB)
-    .send_keys('qatest6').key_down(Keys.TAB).send_keys('Pass0001!').key_down(Keys.TAB).key_down(Keys.TAB).key_down(Keys.ENTER).key_down(Keys.TAB).key_down(Keys.TAB)
-    .send_keys('qatest7').key_down(Keys.TAB).send_keys('Pass0001!').key_down(Keys.TAB).key_down(Keys.TAB).key_down(Keys.ENTER).key_down(Keys.TAB).key_down(Keys.TAB)
-    .send_keys('qatest8').key_down(Keys.TAB).send_keys('Pass0001!').key_down(Keys.TAB).key_down(Keys.TAB).key_down(Keys.ENTER).key_down(Keys.TAB).key_down(Keys.TAB)
-    .send_keys('qatest9').key_down(Keys.TAB).send_keys('Pass0001!').key_down(Keys.TAB).key_down(Keys.TAB).key_down(Keys.ENTER).key_down(Keys.TAB).key_down(Keys.TAB)
-    .send_keys('qatest10').key_down(Keys.TAB).send_keys('Pass0001!').key_down(Keys.TAB).key_down(Keys.TAB).key_down(Keys.ENTER).key_down(Keys.TAB).key_down(Keys.TAB).perform()
- )
+
+user_data = [
+    ('qatest1', 'Pass0001!'),
+    ('qatest2', 'Pass0001!'),
+    ('qatest3', 'Pass0001!'),
+    ('qatest4', 'Pass0001!'),
+    ('qatest5', 'Pass0001!'),
+    ('qatest6', 'Pass0001!'),
+    ('qatest7', 'Pass0001!'),
+    ('qatest8', 'Pass0001!'),
+    ('qatest9', 'Pass0001!'),
+    ('qatest10', 'Pass0001!'),
+]
+for i in range(10):
+    user = user_data[i]
+    if i < 5:
+        action.send_keys(user[0]).key_down(Keys.TAB).send_keys(user[1]).key_down(Keys.TAB).key_down(Keys.TAB).key_down(Keys.TAB).key_down(Keys.TAB)
+    else:
+        action.send_keys(user[0]).key_down(Keys.TAB).send_keys(user[1]).key_down(Keys.TAB).key_down(Keys.TAB).key_down(Keys.ENTER).key_down(Keys.TAB).key_down(Keys.TAB)
+action.perform()
+
+btn_user_apply = driver.find_element(By.CSS_SELECTOR, 'body > app-root > app-sidenav-responsive > div > mat-sidenav-container > mat-sidenav-content > app-user-setting > div > mat-card > div:nth-child(4) > app-btn-apply > button')
+btn_user_apply.click()
 time.sleep(1)
-btn_apply = driver.find_element(By.CSS_SELECTOR, 'body > app-root > app-sidenav-responsive > div > mat-sidenav-container > mat-sidenav-content > app-user-setting > div > mat-card > div:nth-child(4) > app-btn-apply > button')
-btn_apply.click()
-time.sleep(1)
-print('10 user add complte!\n----------------------------------------------------------------')
+print('10 user add OK')
+'''
+# 다른 유저ID로 로그인 확인
 btn_logout = driver.find_element(By.CSS_SELECTOR, 'body > app-root > app-sidenav-responsive > div > mat-toolbar > button:nth-child(4)')
 btn_logout.click()
 
@@ -89,28 +92,23 @@ driver.find_element(By.CLASS_NAME, 'mat-form-field-infix')
 (action.send_keys('qatest10').key_down(Keys.TAB).send_keys('Pass0001!').pause(1).key_down(Keys.ENTER).perform())
 action.reset_actions
 time.sleep(1)
-print('User Login OK\n----------------------------------------------------------------')
+print('User:No.10 Login OK\n----------------------------------------------------------------')
+
 btn_logout = driver.find_element(By.CSS_SELECTOR, 'body > app-root > app-sidenav-responsive > div > mat-toolbar > button:nth-child(4)')
 btn_logout.click()
+
 driver.find_element(By.CLASS_NAME, 'mat-form-field-infix')
 (action.send_keys('admin').key_down(Keys.TAB).send_keys('Pass0001!').pause(1).key_down(Keys.ENTER).perform())
 action.reset_actions
 time.sleep(1)
 driver.get(url) # 네트워크 페이지
 time.sleep(1)
-
-driver.find_element(By.ID, 'mat-checkbox-27').click() # 사용자 제거
-driver.find_element(By.ID, 'mat-checkbox-28').click()
-driver.find_element(By.ID, 'mat-checkbox-29').click()
-driver.find_element(By.ID, 'mat-checkbox-30').click()
-driver.find_element(By.ID, 'mat-checkbox-31').click()
-driver.find_element(By.ID, 'mat-checkbox-32').click()
-driver.find_element(By.ID, 'mat-checkbox-33').click()
-driver.find_element(By.ID, 'mat-checkbox-34').click()
-driver.find_element(By.ID, 'mat-checkbox-35').click()
-driver.find_element(By.ID, 'mat-checkbox-36').click()
-btn_del = driver.find_element(By.CSS_SELECTOR, 'body > app-root > app-sidenav-responsive > div > mat-sidenav-container > mat-sidenav-content > app-user-setting > div > mat-card > mat-list:nth-child(3) > mat-card > div.flex-row > div.ng-star-inserted > app-btn-delete > button')
-btn_del.click()
-print('10 user delete complte!\n----------------------------------------------------------------')
+'''
+# 사용자 전체 제거
+for i in range(13, 23):
+    driver.find_element(By.ID, f'mat-checkbox-{i}').click()
+btn_user_del = driver.find_element(By.CSS_SELECTOR, 'body > app-root > app-sidenav-responsive > div > mat-sidenav-container > mat-sidenav-content > app-user-setting > div > mat-card > mat-list:nth-child(3) > mat-card > div.flex-row > div.ng-star-inserted > app-btn-delete > button')
+btn_user_del.click()
+print('10 user delete OK')
 btn_del_popup = driver.find_element(By.XPATH, '//*[@id="mat-dialog-0"]/app-confirm-dialog/div[2]/button[2]/span')
 btn_del_popup.click() # 사용자 삭제 팝업
