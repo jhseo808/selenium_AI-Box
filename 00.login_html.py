bd040f2727450e8393cd8b29227e576070fe6da9
서정호 | JH$EO <121909004+jhseo808@users.noreply.github.com>
2023-04-11 08:31:20 +0900

    Update and rename 00.html_test.py to 00.login_html.py

--- 00.login_html.py
+++ 00.login_html.py
@@ -11,31 +11,24 @@ import unittest
 import HtmlTestRunner
 import os
 
-# 라이브 및 스트리밍 스트린샷 저장 경로
 try:
     os.makedirs('D:\Auto_test')
     print('Auto테스트 파일이 생성되었습니다.')
 except FileExistsError as a:
     print(a)
 
-# 프린트 로그 파일 저장
-# sys.stdout = open('D:\Auto_test\AI-Box_log.txt','w')
-    
-#브라우저 꺼짐 방지
-options = Options()
-options.add_experimental_option("detach", True)
-service = Service(executable_path=ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service, options=options)
-driver.implicitly_wait(3)
-driver.maximize_window()
 
-# HTML Report 연동 테스트
 class AIBoxTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.driver = driver
-        
-    def test_login(self): # 로그인 테스트
+        options = Options()
+        options.add_experimental_option("detach", True)
+        service = Service(executable_path=ChromeDriverManager().install())
+        cls.driver = webdriver.Chrome(service=service, options=options)
+        cls.driver.implicitly_wait(3)
+        cls.driver.maximize_window()
+       
+    def test_login(self):
         driver = self.driver
         driver.get('http://172.16.6.230/web/#/web/auth')
         action = ActionChains(driver)
@@ -43,12 +36,10 @@ class AIBoxTest(unittest.TestCase):
         (action.send_keys('admin').key_down(Keys.TAB).send_keys('Pass0001!').pause(1).key_down(Keys.ENTER).perform())
         action.reset_actions
         time.sleep(1)
-        print('Login OK')
-        assert "No results found." not in driver.page_source
 
-    def test_osd(self): # osd 활성화 테스트
+    def test_osd(self):
         # 화면 표시 팝업
-        osd_btn = driver.find_element(By.CSS_SELECTOR,\
+        osd_btn = self.driver.find_element(By.CSS_SELECTOR,\
             'body > app-root > app-sidenav-responsive > div > mat-sidenav-container > mat-sidenav-content > app-live > div.flex-container > mat-card:nth-child(3) > div > button'\
             )
         osd_btn.click()
@@ -58,16 +49,17 @@ class AIBoxTest(unittest.TestCase):
             'mat-checkbox-7'
             ]
         for checkbox_id in checkbox_ids:
-            driver.find_element(By.ID, checkbox_id).click()
+            self.driver.find_element(By.ID, checkbox_id).click()
         time.sleep(1)
-        osd_apply_btn = driver.find_element(By.XPATH, '//*[@id="mat-dialog-0"]/display-option-dialog/div/div[2]/button[1]')
+        osd_apply_btn = self.driver.find_element(By.XPATH, '//*[@id="mat-dialog-0"]/display-option-dialog/div/div[2]/button[1]')
         osd_apply_btn.click()
-        print('OSD All enable OK')
-        assert "No results found." not in driver.page_source
-
-    def tearDown(self):
-        pass
-
+    
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+        cls.driver.quit()
+        print('test complete')
+        
 if __name__ == '__main__':
     runner = HtmlTestRunner.HTMLTestRunner(output='report')
     suite = unittest.TestLoader().loadTestsFromTestCase(AIBoxTest)
